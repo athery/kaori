129da9ae007d9601ff3146998a61b6be49c65f1d
AdrienT <adrien.thery@gmail.com>
2018-03-05 23:52:30 +0100

    added litebox to grid

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,8 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
+import "./blueimp-gallery.min"
+
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
@@ -28,4 +30,13 @@ var $grid = $('.grid').masonry({
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
   $grid.masonry();
-});  
+});
+
+document.getElementById('conception-gallery').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
